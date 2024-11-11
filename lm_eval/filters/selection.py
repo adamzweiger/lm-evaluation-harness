baaf6f4e17ac1b14315921e09c0262404ef67992
adamzweiger <adamz@mit.edu>
2024-11-10 20:18:15 -0500

    testing fewshot filtering

--- lm_eval/filters/selection.py
+++ lm_eval/filters/selection.py
@@ -23,6 +23,20 @@ class TakeFirstFilter(Filter):
         return map(lambda r: r[0], resps)
 
 
+@register_filter("take_fourth")
+class TakeFirstFilter(Filter):
+    def __init__(self) -> None:
+        """
+        Can define custom behavior here, if an individual instantiation of a Filter class should have state.
+        """
+
+    def apply(self, resps, docs):
+        """
+        Assuming each entry of `resps` is a list of model responses, we discard all but the first response.
+        """
+        return map(lambda r: r[3], resps)
+
+
 @register_filter("take_first_k")
 class TakeKFilter(Filter):
     def __init__(self, **kwargs) -> None:
